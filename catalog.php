370f1267c945b2955ab1baa7b652f2f76bb00a4c
jaeyoonw <jaeyoon.dev@gmail.com>
2021-08-30 21:42:32 +0900

    php_web

--- catalog.php
+++ catalog.php
@@ -22,12 +22,16 @@ include("inc/header.php"); ?>
 
 <div class="section catalog page">
   <div class="wrapper">
-    <h1><?php echo $pageTitle; ?></h1>
+    <h1><?php
+        if ($section != null) {
+          echo "<a href='catalog.php'>Full Catalog</a> &gt;";
+        }
+        echo $pageTitle; ?></h1>
 
     <ul class="items">
       <?php
-      $categories = array_category($catalog, $section); # $categories에는 리턴한 $output배열의 key가 들어간다.  
-      foreach ($categories as $id) {  # $id는 여러개의 key 들이 된다.
+      $categories = array_category($catalog, $section); # $categories에는 리턴한 key들이 들어있는 배열이 된다.  
+      foreach ($categories as $id) {  # $id는 여러개의 key들이 들어가있는 배열이 된다.
         echo get_item_html($id, $catalog[$id]);
       }
       ?>
